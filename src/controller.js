ba20d3586dd861284c33e0d869ab759935fa2409
S.Amelin <1.slava.1.amelin.1@gmail.com>
2021-12-28 16:37:52 +0200

    Tetris JS

---
+++ src/controller.js
@@ -0,0 +1,84 @@
+class Controller {
+    constructor(game, view) {
+        this.game = game;
+        this.view = view;
+        this.intervalId = null;
+        this.isPlaying = false;
+
+        document.addEventListener('keydown', this.handleKeyDown.bind(this));
+
+        this.view.renderStartScreen();
+    }
+
+    update() {
+        this.game.movePieceDown();
+        this.view.renderMainScreen(this.game.getState());
+    }
+
+    play() {
+        this.isPlaying = true;
+        this.startTimer();
+        this.view.renderMainScreen(this.game.getState());
+    }
+
+    pause() {
+        this.isPlaying = false;
+        this.startTimer();
+        this.view.renderMainScreen(this.game.getState());
+    }
+
+    updateView() {
+        if(!this.isPlaying){
+            this.view.renderPauseScreen();
+        } else {
+            this.view.renderMainScreen(this.game.getState());
+        }
+    }
+
+    startTimer() {
+        const speed = 1000 - this.game.getState().level * 100;
+
+        if(!this.intervalId) {
+            this.intervalId = setInterval(() => {
+                this.update();
+             }, speed > 0 ? speed : 100);
+        }
+    }
+
+    stopTimer() {
+        if(this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    handleKeyDown(event) {
+        switch (event.keyCode){
+            case 13: // Enter arrow
+                if (this.keyCode){
+                    this.pause();
+                } else {
+                    this.play();
+                }
+                break;
+            case 37: // Left arrow
+                this.game.movePieceLeft();
+                this.view.renderMainScreen(game.getState());
+                break;
+            case 38: // Up arrow
+                this.game.rotatePiece();
+                this.view.renderMainScreen(game.getState());
+                break;
+            case 39: // Right arrow
+                this.game.movePieceRigth();
+                this.view.renderMainScreen(game.getState());
+                break;
+            case 40: // Down arrow
+                this.game.movePieceDown();
+                this.view.renderMainScreen(game.getState());
+                break;
+        }
+    }
+}
+
+export default Controller;
---
+++ src/game.js
@@ -0,0 +1,258 @@
+class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200,
+    };
+
+    score = 0;
+    lines = 19;
+
+    playfield = this.createPlayField();
+    activePiece = this.createPiece();
+    nextPiece = this.createPiece();
+
+    get level() {
+        return Math.floor(this.lines * 0.1);
+    }
+
+    getState() {
+        const playfield = this.createPlayField();
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for(let y = 0; y < this.playfield.length; y++) {
+            playfield[y] = [];
+
+            for(let x = 0; x < this.playfield[y].length; x++){
+                playfield[y][x] = this.playfield[y][x];
+            }
+        }
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if(blocks[y][x]){
+                    playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+
+        return {
+            score: this.score,
+            level: this.level,
+            lines: this.lines,
+            nextPiece: this.nextPiece,
+            playfield,
+        };
+    }
+
+    createPlayField() {
+        const playfield = [];
+
+        for(let y = 0; y < 20; y++) {
+            playfield[y] = [];
+
+            for(let x = 0; x < 10; x++){
+                playfield[y][x] = 0;
+            }
+        }
+
+        return playfield;
+    }
+
+    createPiece() {
+        const index = Math.floor(Math.random() * 7);
+        const type = 'IJLOSTZ'[index];
+        const piece = {};
+
+        switch(type) {
+            case 'I':
+                piece.blocks = [
+                    [0, 0, 0, 0],
+                    [1, 1, 1, 1],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                ];
+                break;
+
+            case 'J':
+                piece.blocks = [
+                    [0, 0, 0],
+                    [2, 2, 2],
+                    [0, 0, 2],
+                ];
+                break;
+
+            case 'L':
+                piece.blocks = [
+                    [0, 0, 0],
+                    [3, 3, 3],
+                    [3, 0, 0],
+                ];
+                break;
+
+            case 'O':
+                piece.blocks = [
+                    [0, 0, 0, 0],
+                    [0, 4, 4, 0],
+                    [0, 4, 4, 0],
+                    [0, 0, 0, 0],
+                ];
+                break;
+
+            case 'S':
+                piece.blocks = [
+                    [0, 0, 0],
+                    [0, 5, 5],
+                    [5, 5, 0],
+                ];
+                break;
+
+            case 'T':
+                piece.blocks = [
+                    [0, 0, 0],
+                    [6, 6, 6],
+                    [0, 6, 0],
+                ];
+                break;
+
+            case 'Z':
+                piece.blocks = [
+                    [0, 0, 0],
+                    [7, 7, 0],
+                    [0, 7, 7],
+                ];
+                break;
+            default:
+                throw new Error('Неизвестный тип фигуры');
+        }
+
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+        piece.y = -1;
+
+        return piece;
+    }
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if (this.hasCollision()){
+            this.activePiece.x += 1;
+        }
+    }
+
+    movePieceRigth() {
+        this.activePiece.x += 1;
+
+        if (this.hasCollision()){
+            this.activePiece.x -= 1;
+        }
+    }
+
+    movePieceDown() {
+        this.activePiece.y += 1;
+
+        if (this.hasCollision()){
+            this.activePiece.y -= 1;
+            this.lockPiece();
+            const clearedLines = this.clearedLines();
+            this.updateScore(clearedLines);
+            this.updatePieces();
+        }
+    }
+
+    rotatePiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for(let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
+        }
+
+        for(let y = 0; y < length; y++){
+            for(let x = 0; x < length; x++){
+                temp[x][y] = blocks[length - 1 - y][x]; //меняем строки на столбики
+            }
+        }
+
+        this.activePiece.blocks = temp;
+
+        if(this.hasCollision()) {
+            this.activePiece.blocks = blocks;
+        }
+    }
+
+    hasCollision() {
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for(let y = 0; y < blocks.length; y++){
+            for(let x = 0; x < blocks[y].length; x++){
+                if (blocks[y][x] &&
+                    ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+                    this.playfield[pieceY + y][pieceX + x])
+                ) {return true;}
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+
+        for(let y = 0; y < blocks.length; y++){
+            for(let x = 0; x < blocks[y].length; x++){
+                if (blocks[y][x]){
+                    this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
+
+    clearedLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y => 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if(this.playfield[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if(numberOfBlocks === 0){
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y);
+            }
+        }
+
+        for(let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0));
+        }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        if(clearedLines > 0){
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines += clearedLines;
+            console.log(this.score, this.lines);
+        }
+    }
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
+    }
+}
+
+export default Game;
